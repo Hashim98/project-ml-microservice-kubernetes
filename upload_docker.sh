fa6c5020070b1a2e8ee1c2599f6a2b509bee78b2
Hashim98 <hashimriaz98@outlook.com>
2021-11-02 00:59:29 +0530

    Initial Major Commit

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=hashimriaz98/ml-api:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag ml-api:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
